06de0ba93ce30fd61fa6acb41520dba47bf0ced8
stecala <ste.cala123@gmail.com>
2022-08-15 13:20:27 +0200

    🪛 Chore : import bootstrap and fontawesome

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css);
+@import "~bootstrap/scss/bootstrap";
 :root{
   --title-color : #FA4753;
   --bg-dark : #17181C;
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,12 @@
 <template>
-  <header>
-    CIAO
+  <header class="container-fluid">
+    <div class="row">
+      <div class="col-6 bg-success">
+          <i class="fa-solid fa-ghost"></i>
+      </div>
+      <div class="col-6 bg-danger">
+      </div>
+    </div>
   </header>
 </template>
 
