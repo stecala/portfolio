61500f3cda19547df06cc6cf0f46032245b0bc28
stecala <ste.cala123@gmail.com>
2022-08-15 17:12:46 +0200

    ⭐ Feat (create method) : create method that change the status on click

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,12 +5,14 @@
           <i class="fa-solid fa-ghost"></i>
       </div>
       <div class="col-6 container-hamburger-icon position-relative">
-        <input type="checkbox" id="hi">
-          <label class="menu" for="hi">
-            <div class="bar"></div>
-            <div class="bar"></div>
-            <div class="bar"></div>
-          </label>
+        <div class="d-lg-none" >
+          <input type="checkbox" id="hi" @click="changeStatus()">
+            <label class="menu" for="hi">
+              <div class="bar"></div>
+              <div class="bar"></div>
+              <div class="bar"></div>
+            </label>
+        </div>
       </div>
     </div>
   </header>
@@ -18,7 +20,17 @@
 
 <script>
 export default {
-
+  data : function(){
+    return{
+      isClicked : false,
+    }
+  },
+  methods : {
+    changeStatus(){
+      this.isClicked = !this.isClicked
+      console.warn(this.isClicked)
+    }
+  }
 }
 </script>
 
